1e0f9668bca4ffbd5fcf6ffcf996aae895ca89b4
sirinesbei <sirine.sbei119@gmail119.com>
2026-08-01 15:49:27 +0100

    bd deployed

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,15 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
+import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
   {
-    host: "localhost",
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     dialect: "mysql",
-  },
+    logging: false,
+  }
 );
 
-module.exports = sequelize;
+export default sequelize;
